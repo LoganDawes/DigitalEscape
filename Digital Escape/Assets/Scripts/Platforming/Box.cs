f43dcde6f9bdae7eb1438fc22e1e441226b8fe0e
LoganDawes <logantdawes@gmail.com>
2025-10-11 10:32:53 -0500

    Added Box

--- Digital Escape/Assets/Scripts/Platforming/Box.cs
+++ Digital Escape/Assets/Scripts/Platforming/Box.cs
@@ -7,21 +7,62 @@ using UnityEngine;
  
  */
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
 public class Box : MonoBehaviour
 {
-    // Variables
-
     // Components
+    private Rigidbody2D rb;
+    private MovingPlatform currentPlatform;
 
     // Start
     void Start()
     {
-
+        // Initialize components
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update
-    void Update()
+    // FixedUpdate
+    void FixedUpdate()
     {
+        // If on a moving platform, inherit its velocity
+        if (currentPlatform != null)
+        {
+            rb.linearVelocity += currentPlatform.platformVelocity;
+        }
+    }
 
+    // OnCollisionStay2D
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        // Check if standing on a MovingPlatform
+        var platform = collision.collider.GetComponent<MovingPlatform>();
+        if (platform != null)
+        {
+            foreach (var contact in collision.contacts)
+            {
+                // Only consider contacts from below (standing on top)
+                if (contact.normal.y > 0.5f)
+                {
+                    currentPlatform = platform;
+                    return;
+                }
+            }
+        }
+        else
+        {
+            currentPlatform = null;
+        }
+    }
+
+    // OnCollisionExit2D
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // Reset platform reference when leaving platform
+        if (collision.collider.GetComponent<MovingPlatform>() != null)
+        {
+            currentPlatform = null;
+        }
     }
 }
